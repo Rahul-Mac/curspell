3b56aad327c6c3af6964b56815c16501aba443d4
Rahul Mac <rahulmacwan14@gmail.com>
2025-03-05 22:51:08 +0530

    add currency: ERN, add ext-intl in composer json

---
+++ config/ERN.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Eritrean Nakfa Map
+ */
+return [
+    'subunit' => 100,
+    'en' => [
+        'base' => [
+            'singular' => 'nakfa',
+            'plural' => 'nakfas',
+        ],
+        'fraction' => [
+            'singular' => 'cent',
+            'plural' => 'cents',
+        ],
+        'conjunction' => 'and',
+    ],
+];
