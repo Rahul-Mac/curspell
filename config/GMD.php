11acb508b51f956addff26304ba61fb3d6d078c7
Rahul Mac <rahulmacwan14@gmail.com>
2025-03-08 19:43:49 +0530

    feat: add new currencies

---
+++ config/GMD.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Gambian Dalasi Map
+ */
+return [
+    'subunit' => 100,
+    'en' => [
+        'base' => [
+            'singular' => 'dalasi',
+            'plural' => 'dalasis',
+        ],
+        'fraction' => [
+            'singular' => 'butut',
+            'plural' => 'bututs',
+        ],
+        'conjunction' => 'and',
+    ],
+];
---
+++ config/GNF.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Guinean Franc Map
+ */
+return [
+    'subunit' => 100,
+    'en' => [
+        'base' => [
+            'singular' => 'franc',
+            'plural' => 'francs',
+        ],
+        'fraction' => [
+            'singular' => 'centime',
+            'plural' => 'centimes',
+        ],
+        'conjunction' => 'and',
+    ],
+];
